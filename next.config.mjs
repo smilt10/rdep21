42b06a230e1066d52f7d8868f39e5e960b2496df
smilt10 <gideon.owusu3057@gmail.com>
2025-06-06 14:01:48 +0200

    deployment annex

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✅ this tells Next.js to generate static files
+  basePath: '/rdep21', // ✅ important for GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
